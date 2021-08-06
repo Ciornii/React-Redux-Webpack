596668dd36f82ce0df3d8b606a20624d9ef5b240
Victor Ciornii <ciorniivictor96@gmail.com>
2021-08-06 16:40:30 +0300

    Update Repo.jsx

--- src/components/main/repo/Repo.jsx
+++ src/components/main/repo/Repo.jsx
@@ -9,10 +9,10 @@ const Repo = (props) => {
         <div className="repo">
             <div className="repo-header">
                 <div className="repo-header-name"><NavLink to={`/card/${repo.owner.login}/${repo.name}`}>{repo.name}</NavLink></div>
-                <div className="repo-header-stars">Количество звезд: {repo.stargazers_count}</div>
+                <div className="repo-header-stars">Stars: {repo.stargazers_count}</div>
             </div>
-            <div className="repo-last-commit">Последний коммит: {repo.updated_at}</div>
-            <a href={repo.html_url} className="repo-link">Ссылка на репозиторий: {repo.html_url}</a>
+            <div className="repo-last-commit">Last commit: {repo.updated_at}</div>
+            <a href={repo.html_url} className="repo-link">Link: {repo.html_url}</a>
         </div>
     );
 };
